52f238dc2c5ca7007a6eaef73d9052008ce0b49c
mm <me@marinamiranda.net>
2016-03-11 23:21:06 -0500

    commit

--- este.js
+++ este.js
@@ -25,38 +25,46 @@ app.controller('mainCtlr', function ($scope,$location,$routeParams) {
   // Listado de eventos:
   $scope.shows = [{
     "id": 0,
-    "title": "Buscando Invisibles",
-    "artist": "David Garcia Casado / Book launch",
-    "location": "La Fabrica, Madrid - Friday January 15 2016",
-    "location2": "",
-    "image": [
-      {'img': 'imgs/Invisibles.jpg', 'titulo': 'Buscando Invisibles', 'autor': 'David Garcia Casado', 'materiales': '','fecha': '2016'},
-      //{'img': 'imgs/Invisibles.jpg', 'titulo': 'Buscando Invisibles', 'autor': 'David Garcia Casado', 'materiales': '142pg','fecha': '2016'}
+    "title": "Wrong Doing",
+    "artist": "Jiajia Zhang, Victor Esther",
+    "location": "February 13 - March 25 2016" ,
+    "location2": "" ,
+    "image" : [
+      {'img': 'imgs/Wrong_Doing_View_01.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/Wrong_Doing_View_02.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/Wrong_Doing_View_03.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/Wrong_Doing_YesSolid_Plot_JJZ_a.jpg', 'titulo': 'Jiajia Zhang, 2016', 'autor': 'Yes, Solid (Plot)', 'materiales':'Sharpie on printed adhesive vinyl, acrylic glass, wall mount.','fecha': ''},
+      {'img': 'Wron_Doing_Visitor_JJZ_2016.jpg', 'titulo': 'Jiajia Zhang, 2016', 'autor': 'Visitor', 'materiales':'C-print, artist frame.','fecha': ''},
+      {'img': 'imgs/Wrong_Doing_ SpineS_VE.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Spine (Frieze - October 2015), Spine (Artforum - February 2016)', 'materiales': '', 'fecha': 'Spine (Frieze - October 2014), Spine (Frieze - February 2016)'},
+      {'img': 'imgs/Spine_AF_AA.jpg', 'titulo': '#Spine (Artforum - November 2013)', 'autor': '#Spine (Art in America - February 2016)', 'materiales': '','fecha': ''},
+
+
     ]
   },
     {
       "id": 1,
-      "title": "Wrong Doing",
-      "artist": "Jiajia Zhang, Victor Esther",
-      "location": "February 13 - March 25 2016" ,
-      "location2": "" ,
-      "image" : [
-        {'img': 'imgs/EsteShow.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''}
+      "title": "Buscando Invisibles",
+      "artist": "David Garcia Casado / Book launch",
+      "location": "La Fabrica, Madrid - Friday January 15 2016",
+      "location2": "",
+      "image": [
+        {'img': 'imgs/Invisibles.jpg', 'titulo': 'Buscando Invisibles', 'autor': 'A collection of essays by David García Casado', 'materiales': 'Spanish edition, 143p','fecha': 'Order by email - $15'},
+        //{'img': 'imgs/Invisibles.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': 'January 2016'}
 
       ]
     }
   ];
 
   // ---------------------------------------------------------------
-    $scope.showEvento = function(show){
-      console.log(show);
-      $scope.showEventoIndividual = true;
-      for(var x = 0; x< $scope.shows.length; x++){
-        if(show == $scope.shows[x].id){
-          $scope.current = $scope.shows[x];
-        }
+  $scope.showEvento = function(show){
+    console.log(show);
+    $scope.showEventoIndividual = true;
+    for(var x = 0; x< $scope.shows.length; x++){
+      if(show == $scope.shows[x].id){
+        $scope.current = $scope.shows[x];
       }
     }
+  }
   $scope.inicio = function(){
     console.log("inicio");
     $scope.showEventoIndividual = false;
