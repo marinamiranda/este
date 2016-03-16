d5d9f18310d28af996a26402a8ba60b6f2979207
mm <me@marinamiranda.net>
2016-03-16 11:45:39 -0400

    changes

--- este.js
+++ este.js
@@ -30,13 +30,28 @@ app.controller('mainCtlr', function ($scope,$location,$routeParams) {
     "location": "February 13 - March 25 2016" ,
     "location2": "" ,
     "image" : [
-      {'img': 'imgs/Wrong_Doing_View_01.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
-      {'img': 'imgs/Wrong_Doing_View_02.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
-      {'img': 'imgs/Wrong_Doing_View_03.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
-      {'img': 'imgs/Wrong_Doing_YesSolid_Plot_JJZ_a.jpg', 'titulo': 'Jiajia Zhang, 2016', 'autor': 'Yes, Solid (Plot)', 'materiales':'Sharpie on printed adhesive vinyl, acrylic glass, wall mount.','fecha': ''},
-      {'img': 'imgs/Wron_Doing_Visitor_JJZ_2016.jpg', 'titulo': 'Jiajia Zhang, 2016', 'autor': 'Visitor', 'materiales':'C-print, artist frame.','fecha': ''},
-      {'img': 'imgs/Wrong_Doing_ SpineS_VE.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Spine (Frieze - October 2015), Spine (Artforum - February 2016)', 'materiales': '', 'fecha': 'Spine (Frieze - October 2014), Spine (Frieze - February 2016)'},
-      {'img': 'imgs/Spine_AF_AA.jpg', 'titulo': '#Spine (Artforum - November 2013)', 'autor': '#Spine (Art in America - February 2016)', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_view_01.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_view_02.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_window_1.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_YesSolid_Plot_JJZ_2016_a.jpg', 'titulo': 'Jiajia Zhang, 2016 (In collaboration with a Mexican Singwriter)', 'autor': 'Yes, Solid (Plot)', 'materiales':'Sharpie, adhesive vinyl, acrylic glass, wall mount.','fecha': ''},
+      {'img': 'imgs/WD_YesSolid_Plot_Detail_JJZ.jpg', 'titulo': 'Jiajia Zhang, 2016,', 'autor': 'Yes, Solid (Plot)', 'materiales':'(Detail).','fecha': ''},
+      {'img': 'imgs/WD_Definition_2016.jpg', 'titulo': 'Jiajia Zhang - Victor Esther, 2016', 'autor': 'Definition', 'materiales': 'Drywall, wall mount. ','fecha': ''},
+      {'img': 'imgs/WD_Spine_Two_VE.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Spine (Artforum - November 2013) ', 'materiales': 'Spine (Art in America - February 2016) ','fecha': 'Magazines, wall mount.'},
+      {'img': 'imgs/WD_ Math&Biology_VE_2016.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Math & Biology', 'materiales': '','fecha': 'Magazine covers, office supplies, hardware.'},
+      {'img': 'imgs/WD_Math&Biology_detail.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Math & Biology', 'materiales': '(Detail).','fecha': ''},
+      { 'img': 'imgs/WD_View_03.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_Visitor_JJZ_2016.jpg', 'titulo': 'Jiajia Zhang, 2016', 'autor': 'Visitor', 'materiales': 'C-print, artist frame.','fecha': ''},
+      {'img': 'imgs/WD_YesSolid_Snails_JJZ_2016.jpg', 'titulo': 'Jiajia Zhang, 2016 (In collaboration with a Chinese Singwriter)', 'autor': 'Yes, Solid (Snails)', 'materiales': 'Sharpie, adhesive  vinyl, acrylic glass, wall mount.','fecha': ''},
+      {'img': 'imgs/WD_JJZ_Resident_view_2016.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_JJZ_Resident_2016.jpg', 'titulo': 'Jiajia Zhang, 2016', 'autor': 'Resident', 'materiales': 'C-print, artist frame.','fecha': ''},
+      {'img': 'imgs/WD_ TExt&Image_VE_S.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Text & Image', 'materiales': 'Custom made earrings.','fecha': ''},
+      {'img': 'imgs/WD_IamNobody_View.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_Iamnobody_2016.jpg', 'titulo': 'Jiajia Zhang - Victor Esther, 2016', 'autor': 'I am Nobody, who are you?', 'materiales': '(Nobody asks the poet why he wrote the poem)','fecha': 'Drywall, hardware, floor & patio paint.'},
+      {'img': 'imgs/WD_ Spines_VE_Four_2016.jpg', 'titulo': 'Victor Esther, 2016', 'autor': 'Spine (Frieze - October 2015),Spine (Artforum - February 2016),', 'materiales': 'Spine (Frieze - October 2014), Spine (Frieze - February 2016)','fecha': 'Magazines, wall mount.'},
+      {'img': 'imgs/WD_ Add_VE_2016.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': ''},
+      {'img': 'imgs/WD_Add_VE_Detail_2016.jpg', 'titulo': 'Victor Esther, 2012', 'autor': 'Ad (Artforum)', 'materiales': '','fecha': 'Tempera on magazine page, frame, wall mount.'},
+
+
 
 
     ]
@@ -48,7 +63,7 @@ app.controller('mainCtlr', function ($scope,$location,$routeParams) {
       "location": "La Fabrica, Madrid - Friday January 15 2016",
       "location2": "",
       "image": [
-        {'img': 'imgs/Invisibles.jpg', 'titulo': 'Buscando Invisibles', 'autor': 'A collection of essays by David García Casado', 'materiales': 'Spanish edition, 143p','fecha': 'Order by email - $15'},
+        {'img': 'imgs/Invisibles.jpg', 'titulo': 'Buscando Invisibles, 2016', 'autor': 'A collection of essays by David García Casado', 'materiales': 'Spanish edition, 143p','fecha': 'order by email - $15'},
         //{'img': 'imgs/image.jpg', 'titulo': '', 'autor': '', 'materiales': '','fecha': 'January 2016'},
 
       ]
